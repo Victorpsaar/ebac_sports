3991a0ff568fe4ff0ce47772d1bda613f7f4093c
Victor Porto Saar <victorpsaar@gmail.com>
2024-08-20 15:14:49 -0300

    Criação da Api para requisição HTTP

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,17 +1,15 @@
-import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
 
 import * as S from './styles'
 
-type Props = {
-  produtos: ProdutoType[]
-}
+const ProdutosComponent = () => {
+  const { data: produtos } = useGetProdutosQuery()
 
-const ProdutosComponent = ({ produtos }: Props) => {
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto key={produto.id} produto={produto} />
         ))}
       </S.Produtos>
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import carrinhoReducer from './reducers/carrinho'
 import favoritoReducer from './reducers/favorito'
+import { Api } from '../services/api'
 
 export const store = configureStore({
   reducer: {
     carrinho: carrinhoReducer,
-    favorito: favoritoReducer
-  }
+    favorito: favoritoReducer,
+    [Api.reducerPath]: Api.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(Api.middleware)
 })
 
 export type RootReducer = ReturnType<typeof store.getState>
